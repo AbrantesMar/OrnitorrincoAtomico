666d335822c8c9c9bdf5d948142ddfd83d90ba9a
AbrantesMar <abs.marcio@gmail.com>
2016-07-19 06:28:06 -0300

    solucao da solucao do projeto

---
+++ Mobile/OrnitorrincoSK8/OrnitorrincoSK8/OrnitorrincoSK8Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace OrnitorrincoSK8
+{
+	public partial class OrnitorrincoSK8Page : ContentPage
+	{
+		public OrnitorrincoSK8Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
